6dbf4bd7214361ef319e7dc19edd5297fb3db17b
Tayeb Abderahim Ismail <ita27rmp100@gmail.com>
2024-11-16 10:48:37 +0100

    some new changes on landing page's template
    adding new part , solve some issues in the responsive and edit somethings I didn't like

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// card's tag
-customElements.define('add-card',class extends HTMLElement{
+// course's tag
+customElements.define('new-course',class extends HTMLElement{
     connectedCallback(){
         let title = this.getAttribute('title')
         let details = this.getAttribute('details')
@@ -18,12 +18,72 @@ customElements.define('add-card',class extends HTMLElement{
         this.setAttribute('class','col')
     }
 })
+// goal's tag
+customElements.define('new-goal',class extends HTMLElement{
+    connectedCallback(){
+        let title = this.getAttribute('title')
+        let details = this.getAttribute('details')
+        this.innerHTML =`
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>${title}</h5>
+                        </div>
+                        <div class="card-body" style="text-align:justify">${details}</div>
+                    </div>`
+        this.setAttribute('class','col-lg m-3')
+    }
+})
+// service's tag
+customElements.define('new-service',class extends HTMLElement{
+    connectedCallback(){
+        let img = this.getAttribute('img')
+        this.innerHTML = `
+            <div class="card">
+                <img src="images/services/${img}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <a href="${this.getAttribute('src')}" class="btn btn-primary social">اطلب الخدمة</a>
+                </div>
+            </div>`
+        this.setAttribute('class','col m-2')
+    }
+})
+// member's tag
+customElements.define('new-member',class extends HTMLElement{
+    connectedCallback(){
+        let img = this.getAttribute('image');
+        let name = this.getAttribute('name');
+        let tasks = this.getAttribute('tasks');
+        let cv_link = this.getAttribute('cvlink');
+        this.innerHTML = `
+            <div >
+                <img class="card-img-top rounded-circle" style="width:250px;height:250px;" src="/images/members/${img}" alt="Card image cap">
+                <div class="pt-3">
+                  <h5 class="card-title">${name}</h5>
+                  <p class="card-text">${tasks}</p>
+                  <a href="${cv_link}" class="btn btn-primary" data-toggle="modal" data-target="#modal">سيرته الذاتية</a>
+                </div>
+            </div>`
+        this.setAttribute('class','col m-2')
+    }
+})
+// statistics 
+customElements.define('new-ach',class extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML=`<div class="card bg-transparent text-light stCard">
+                            <div class="card-body">
+                            <h3  id=${this.getAttribute('idCild')} >${this.getAttribute('value')}</h3>
+                            <h6>${this.getAttribute('of')}</h6>
+                            </div>
+                        </div>`
+        this.setAttribute('class','col m-3')
+    }
+})
 // jQuery code
     // scrolling settings
 $(window).scroll(
     function() {
         let height = $('body').css('height').replace('px','')
-        let progress = (pageYOffset*100)/height
+        let progress = (pageYOffset*150)/height
         $('#loading').attr('aria-valuenow',progress)
                     .css('width',`${progress}%`)
         if (pageYOffset>='300') {
@@ -31,8 +91,30 @@ $(window).scroll(
         } else {
             $('#arrow').fadeOut(1500)
         }
+        if (pageYOffset>='2350') {
+            setTimeout(() => {
+                $('id').fadeOut()
+            }, 1000);
+        }
     }
 )
+    // statistics counter
+function counter(id) {
+    let value = Number($(id).text())+1 , i = 0 , count
+    console.log(value)
+    $(id).text('0')
+    count = setInterval(() => {
+            $(id).text(`${i}`)
+            i++
+        },50)
+    setTimeout(() => {
+        clearInterval(count)
+    },50*value);
+}
+counter('#students')
+counter('#courses')
+counter('#lectures')
+counter('#consulting')
 $(document).ready(
     function() {
         //  social media settings
@@ -41,6 +123,8 @@ $(document).ready(
             'data-target':'#modal',
             'target':'_blank'
         }).addClass('btnLink');
+        // statistics
+        $('.status').slideDown()
         //  events
         $('.btnLink').click(
             function(params) {
@@ -56,21 +140,10 @@ $(document).ready(
                 window.location = '#'
             }
         )
-        $('#search').keyup(
-            function(){
-                let search = $(this).val()
-                $('#list tr').filter(
-                    function() {
-                        let resualt = $(this).text().indexOf(search)
-                        $(this).toggle($(this).text().indexOf(search) > -1)
-                        console.log(`search : ${search}`,`resualt : ${resualt}`)
-                    }
-                )
-            }
-        )
-        $('.chapter').addClass('pt-3 pb-3 text-center')
+        $('.chapter').addClass('pt-4 pb-4 h-75 text-center')
+        $('label').addClass('p-1 ')
+        // courses
+        $('.headerCourse').fadeTo(500,1)
+        $('body').fadeTo(5,1)
     }
 )
-
-
-
